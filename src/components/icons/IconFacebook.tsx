5940a373581e300f04d5be29f145b0b2547d1059
phamthuy <phamthithuy1320@gmail.com>
2023-02-25 23:50:53 +0700

    Fix lint

--- src/components/icons/IconFacebook.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { HTMLProps } from "react";
-
-export const IconFacebook = (props: HTMLProps<HTMLImageElement>) => {
-  return (
-    <img src={"icons/fb-icon.png"} alt={"fb-icon"} width={"50px"} {...props} />
-  );
-};
